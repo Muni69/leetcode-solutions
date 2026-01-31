9b2b95978b06654e603a33d6a30fd02f4cc43a29
Muni69 <mrunaldesai28@gmail.com>
2026-01-31 15:00:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (33.08%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+    int count =0;
+    for(int i=s.length()-1;i>=0;i--) 
+    { 
+        if(s.at(i)==' ') 
+        { 
+          if(count>0) 
+          { 
+            return count;
+          }
+        }
+        else 
+        { 
+            count++;
+        }
+    }
+    return count;
+    }
+};
